464f3aa79dabffe46a5ab9f1600c3a845fec97ab
mrch-hub <randycaesario@gmail.com>
2022-05-10 18:13:06 +0300

    Update notebook (enhancing readability) and add some helper functions in the watt modules

--- notebook/watt/utils.py
+++ notebook/watt/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 class SSDataset(torch.utils.data.Dataset):
     def __init__(self, encodings):
@@ -7,3 +8,19 @@ class SSDataset(torch.utils.data.Dataset):
         return {key: val[idx].clone().detach() for key, val in self.encodings.items()}
     def __len__(self):
         return len(self.encodings['inputs_embeds'])
+
+class WirelineDataset(torch.utils.data.Dataset):
+    def __init__(self, data, seq_len):
+        self.data = data
+        self.seq_len = seq_len
+    def __getitem__(self, idx):
+
+        well = np.unique(self.data.WELL.values[idx*self.seq_len:(idx+1)*self.seq_len])[0]
+        data = self.data.drop(columns=['DTS']).values[idx*self.seq_len:(idx+1)*self.seq_len]
+        label = self.data['DTS'].values[idx*self.seq_len:(idx+1)*self.seq_len].reshape(-1,1)
+
+        wells = {'well': well, 'data': data, 'label':label}
+
+        return wells
+    def __len__(self):
+        return int(len(self.data)/self.seq_len)
